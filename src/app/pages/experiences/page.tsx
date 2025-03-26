90127020f60a8577480034209161c944162adb1b
CaioCozendey <caiocozendey@hotmail.com>
2025-03-26 09:18:11 -0300

    add: experiencia

--- src/app/pages/experiences/page.tsx
+++ src/app/pages/experiences/page.tsx
@@ -24,6 +24,18 @@ const Experiences = () => {
       skills: ["Python", "Selenium", "Tesseract", "Pyautogui", "Pandas", "Git"],
       href: "/experience/treeinova",
       logoClass: "bg-gray-200"
+    },
+    sogamax: {
+      logo: "/icons/logo/sogamax.png",
+      skills: ["Python", "Selenium", "Pandas", "Reactjs", "Tailwind", "Typescript"],
+      href: "/experience/sogamax",
+      logoClass: "bg-gray-200"
+    },
+    highJump: {
+      logo: "/icons/logo/highjump.png",
+      skills: ["Python", "Selenium", "Pandas", "Reactjs", "Tailwind", "Typescript"],
+      href: "/experience/sogamax",
+      logoClass: "bg-gray-200"
     }
   };
 
@@ -43,6 +55,20 @@ const Experiences = () => {
         period: "Oct 2023 - Jan 2023",
         position: "RPA Developer",
         description: "Automated business processes using Python, Selenium and Pyautogui, reducing manual workload by 60% and improving operational efficiency.",
+      },
+      {
+        ...commonExperienceProps.sogamax,
+        company: "Sogamax Distribuidora",
+        period: "Set 2023 - Oct 2023 (1 month)",
+        position: "IT Intern",
+        description: "Developed automation solutions with Python and Selenium while contributing to web development projects using React. Enhanced operational efficiency by 40% through RPA bots and improved user interfaces.",
+      },
+      {
+        ...commonExperienceProps.highJump,
+        company: "High Jump",
+        period: "Set 2023 - Oct 2023 (1 month)",
+        position: "IT Intern",
+        description: "Developed automation solutions with Python and Selenium while contributing to web development projects using React. Enhanced operational efficiency by 40% through RPA bots and improved user interfaces.",
       }
     ],
     pt: [
@@ -59,6 +85,20 @@ const Experiences = () => {
         period: "Out 2023 - Jan 2025",
         position: "Desenvolvedor RPA",
         description: "Automatizei processos empresariais utilizando Python, Selenium e Pyautogui, reduzindo a carga de trabalho manual em 60% e melhorando a eficiência operacional.",
+      },
+      {
+        ...commonExperienceProps.sogamax,
+        company: "Sogamax Distribuidora",
+        period: "Set 2023 - Out 2023 (1 mês)",
+        position: "Desenvolvedor RPA",
+        description: "Desenvolvi soluções de automação com Python e Selenium enquanto contribuía para projetos web com React. Aumentei a eficiência operacional em 40% através de bots RPA e melhorei interfaces de usuário.",
+      },
+      {
+        ...commonExperienceProps.highJump,
+        company: "High Jump",
+        period: "Set 2023 - Out 2023 (1 mês)",
+        position: "Desenvolvedor RPA",
+        description: "Desenvolvi soluções de automação com Python e Selenium enquanto contribuía para projetos web com React. Aumentei a eficiência operacional em 40% através de bots RPA e melhorei interfaces de usuário.",
       }
     ]
   };
